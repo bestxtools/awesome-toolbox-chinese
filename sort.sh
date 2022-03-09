a8ef801cfa37872001c0014ed0791a1973168ee2
pipecraft <pipecraft.net@gmail.com>
2022-03-09 09:17:45 +0800

    Display total count of items

--- sort.sh
+++ sort.sh
@@ -1,7 +1,11 @@
 grep -E -o "^\- \[.*?\]" README.md | grep -v "（" > ._tmp1
 grep -E -o "^\- \[.*?\]" README.md | grep -v "（" | sort -f > ._tmp2
 cat ._tmp2
+
+echo 
+echo Total: `wc -l ._tmp2 | grep -E -o "\d+ "`
 echo 
 echo Diff:
+
 diff ._tmp1 ._tmp2
 rm -f ._tmp1 ._tmp2
